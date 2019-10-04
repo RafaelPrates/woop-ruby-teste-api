fb8b39681f08fa2b6e634e2c7554f8c26b889c1c
Rafael de Souza Prates <pratesr@dbserver.com.br>
2019-10-04 19:08:46 -0300

    Finalizando cucumber env e adicionando report ao repositório

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,9 +3,11 @@ require "cucumber"
 require "rspec"
 require "json-schema"
 
-HTTParty::Basement.default_options.update(verify: false)
 
+HTTParty::Basement.default_options.update(verify: false)
 
 api_configs = YAML.load_file('./features/support/api.yml')
 
-$api = api_configs['local']
+$profile = ENV['PROFILE']
+
+$api = api_configs[$profile]
